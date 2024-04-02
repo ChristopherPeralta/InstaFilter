3a84b865d207a9dadb4d8c75369d056741bc1ae6
Christopher Peralta <114698519+ChristopherPeralta@users.noreply.github.com>
2024-04-02 08:50:52 -0500

    .

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,20 +5,59 @@
 //  Created by Christopher Peralta on 26/03/24.
 //
 
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            VStack {
+                Spacer()
+
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+
+                    Spacer()
+
+                    
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            }
     }
 }
+
+func changeFilter() {
+    
+}
+
+    
+//https://www.hackingwithswift.com/books/ios-swiftui/importing-an-image-into-swiftui-using-photospicker
 
 #Preview {
     ContentView()
 }
+
+
